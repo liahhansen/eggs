beb8a0dc1be7cd34049780c499d01b9740e427a7
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-24 12:21:37 -0800

    test for saving farm w/o name

--- spec/models/farm_spec.rb
+++ spec/models/farm_spec.rb
@@ -11,4 +11,9 @@ describe Farm do
   it "should create a new instance given valid attributes" do
     Farm.create!(@valid_attributes)
   end
+
+  it "should not allow saving of a new instance without name" do
+    f = Farm.new
+    f.valid?.should == false
+  end
 end
