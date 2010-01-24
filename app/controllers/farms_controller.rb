551afe13adf17262ba0f38690d92481e4e2af5be
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-24 12:08:26 -0800

    added farm model

---
+++ app/controllers/farms_controller.rb
@@ -0,0 +1,85 @@
+class FarmsController < ApplicationController
+  # GET /farms
+  # GET /farms.xml
+  def index
+    @farms = Farm.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @farms }
+    end
+  end
+
+  # GET /farms/1
+  # GET /farms/1.xml
+  def show
+    @farm = Farm.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @farm }
+    end
+  end
+
+  # GET /farms/new
+  # GET /farms/new.xml
+  def new
+    @farm = Farm.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @farm }
+    end
+  end
+
+  # GET /farms/1/edit
+  def edit
+    @farm = Farm.find(params[:id])
+  end
+
+  # POST /farms
+  # POST /farms.xml
+  def create
+    @farm = Farm.new(params[:farm])
+
+    respond_to do |format|
+      if @farm.save
+        flash[:notice] = 'Farm was successfully created.'
+        format.html { redirect_to(@farm) }
+        format.xml  { render :xml => @farm, :status => :created, :location => @farm }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @farm.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /farms/1
+  # PUT /farms/1.xml
+  def update
+    @farm = Farm.find(params[:id])
+
+    respond_to do |format|
+      if @farm.update_attributes(params[:farm])
+        flash[:notice] = 'Farm was successfully updated.'
+        format.html { redirect_to(@farm) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @farm.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /farms/1
+  # DELETE /farms/1.xml
+  def destroy
+    @farm = Farm.find(params[:id])
+    @farm.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(farms_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ db/migrate/20100124195858_create_farms.rb
@@ -0,0 +1,13 @@
+class CreateFarms < ActiveRecord::Migration
+  def self.up
+    create_table :farms do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :farms
+  end
+end
