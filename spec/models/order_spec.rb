25018507f19240b10ac9609931e67f64bd9fb138
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-24 23:21:19 -0800

    some fixture data for orders

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Order do
+  before(:each) do
+    @valid_attributes = {
+      
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Order.create!(@valid_attributes)
+  end
+end
