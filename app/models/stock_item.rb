645727387b59925f0110f4a5823810e3558cdfe9
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-29 23:50:16 -0800

    added sold_out? to stock_item

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -2,4 +2,19 @@ class StockItem < ActiveRecord::Base
   belongs_to :pickup
   belongs_to :product
   has_many :order_items
+
+
+  def sold_out?
+    # total += item.stock_item.product.price * item.quantity
+
+    items = OrderItem.find_all_by_stock_item_id(id)
+
+    total_ordered = 0
+    items.each do |item|
+      total_ordered += item.quantity
+    end
+
+    total_ordered >= quantity_available ? true : false
+    
+  end
 end
--- spec/models/stock_item_spec.rb
+++ spec/models/stock_item_spec.rb
@@ -10,4 +10,13 @@ describe StockItem do
   it "should create a new instance given valid attributes" do
     StockItem.create!(@valid_attributes)
   end
+
+  it "should return sold out status" do
+    eggs = stock_items(:feb3_eggs)
+    chicken = stock_items(:feb3_chickenreg)
+
+    eggs.sold_out?.should == true
+    chicken.sold_out?.should == false
+  end
+
 end
