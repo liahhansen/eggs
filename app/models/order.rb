37e5e7fddd85485fc2f9759b0ff1728091c1d537
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-31 18:46:55 -0800

    added validations to order for existing member/pickup ids

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,23 @@ class Order < ActiveRecord::Base
   belongs_to :pickup
   has_many :order_items, :dependent => :destroy
 
+  validates_presence_of :member_id, :pickup_id
+  validate :member_must_exist, :pickup_must_exist
+
   def estimated_total
     total = 0
     order_items.each do |item|     
       total += item.stock_item.product.price * item.quantity
     end
     total
+  end
+
+  def member_must_exist
+    errors.add(:member_id, "this member must exist") if member_id && !Member.find(member_id)
+  end
+
+  def pickup_must_exist
+    errors.add(:pickup_id, "this pickup must exist") if pickup_id && !Pickup.find(pickup_id)
   end
   
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,15 @@
+require 'spec'
 require 'spec_helper'
 
 describe Order do
   before(:each) do
+    @k_order = orders(:kathryn_sf_emeryville_feb3)
+    @k_member = members(:kathryn)
+    @e_pickup = pickups(:sf_emeryville_feb3)
+
     @valid_attributes = {
-      
+      :member_id => @k_member.id,
+      :pickup_id => @e_pickup.id
     }
   end
 
@@ -11,13 +17,25 @@ describe Order do
     Order.create!(@valid_attributes)
   end
 
+  it "should raise errors when saving when no member or pickup is specified" do
+    Order.new.valid?.should == false
+    lambda {Order.create!}.should raise_error
+    lambda {Order.create!(:member_id => 1)}.should raise_error
+    lambda {Order.create!(:pickup_id => 2)}.should raise_error
+  end
+
+  it "should only accept valid member and pickup ids" do
+    lambda {Order.new(:member_id => 235, :pickup_id => 235).valid?}.should raise_error
+    Order.new(:member_id => @k_member.id, :pickup_id => @e_pickup.id).valid?.should == true    
+  end
+
   it "should return an estimated order total" do
-    order= orders(:kathryn_sf_emeryville_feb3)
+    order= @k_order
     order.estimated_total.should == 91    
   end
 
   it "should destroy order_items when destroying order" do
-    order = orders(:kathryn_sf_emeryville_feb3)
+    order = @k_order
     order.destroy.frozen?.should == true
 
     order.order_items.each do |item|
