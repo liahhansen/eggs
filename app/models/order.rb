bbeb989e0aec05fb9cf6f10e4b970a32615284c0
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-02-01 00:11:02 -0800

    Added minimum order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ActiveRecord::Base
   has_many :order_items, :dependent => :destroy
 
   validates_presence_of :member_id, :pickup_id
-  validate :member_must_exist, :pickup_must_exist
+  validate :member_must_exist, :pickup_must_exist, :total_meets_minimum
 
   accepts_nested_attributes_for :order_items, :reject_if => lambda {|item| item["quantity"].to_i == 0 || item["quantity"].to_i == nil}
 
@@ -22,6 +22,14 @@ class Order < ActiveRecord::Base
 
   def pickup_must_exist
     errors.add(:pickup_id, "this pickup must exist") if pickup_id && !Pickup.find(pickup_id)
+  end
+
+  def total_meets_minimum
+    if pickup
+      if pickup.minimum_order_total
+        errors.add_to_base("your order does not meet the minimum") if estimated_total < pickup.minimum_order_total
+      end
+    end
   end
   
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,14 +3,32 @@ require 'spec_helper'
 
 describe Order do
   before(:each) do
-    @k_order = orders(:kathryn_sf_emeryville_feb3)
-    @k_member = members(:kathryn)
-    @e_pickup = pickups(:sf_emeryville_feb3)
+
+    @kathryn_order = orders(:kathryn_sf_emeryville_feb3)
+    @kathryn = members(:kathryn)
+    @emeryville_pickup = pickups(:sf_emeryville_feb3)
 
     @valid_attributes = {
-      :member_id => @k_member.id,
-      :pickup_id => @e_pickup.id
+      :member_id => @kathryn.id,
+      :pickup_id => @emeryville_pickup.id,
+      :order_items => get_order_items
     }
+  end
+
+
+  def get_order_core
+    return Order.new(:member_id => @kathryn.id, :pickup_id => @emeryville_pickup.id)
+  end
+
+  def get_order_items
+    return [order_items(:kathryn_feb3_chickenreg),order_items(:kathryn_feb3_eggs)]
+  end
+
+  def get_valid_order
+    o = get_order_core
+    o.order_items << get_order_items[0]
+    o.order_items << get_order_items[1]
+    return o
   end
 
   it "should create a new instance given valid attributes" do
@@ -26,21 +44,32 @@ describe Order do
 
   it "should only accept valid member and pickup ids" do
     lambda {Order.new(:member_id => 235, :pickup_id => 235).valid?}.should raise_error
-    Order.new(:member_id => @k_member.id, :pickup_id => @e_pickup.id).valid?.should == true    
+    get_valid_order.valid?.should == true    
   end
 
   it "should return an estimated order total" do
-    order= @k_order
-    order.estimated_total.should == 91    
+    order= @kathryn_order
+    order.estimated_total.should == 91
   end
 
   it "should destroy order_items when destroying order" do
-    order = @k_order
+    order = @kathryn_order
     order.destroy.frozen?.should == true
 
     order.order_items.each do |item|
       item.frozen?.should == true
     end
+  end
+
+  it "should not be valid unless the order total is greater than the pickup minimum" do
+    o = get_order_core
+    o.order_items << order_items(:kathryn_feb3_eggs)
+    o.valid?.should == false
+    o.errors.on_base.should == "your order does not meet the minimum"
+
+    o.order_items << order_items(:kathryn_feb3_chickenreg)
+    o.valid?.should == true
+
   end
 
 end
