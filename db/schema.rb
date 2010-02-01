a65d3a30385972eaba01ed5dce81003afc7eff49
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-31 16:01:00 -0800

    added a hide attr to StockItem because deleting would break OrderItems

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100130060401) do
+ActiveRecord::Schema.define(:version => 20100131235925) do
 
   create_table "farms", :force => true do |t|
     t.string   "name"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20100130060401) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "hide"
   end
 
   create_table "subscriptions", :force => true do |t|
