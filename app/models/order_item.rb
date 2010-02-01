450132d71357cde30e1ff0a44e9e9af6331f9e2c
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-01-31 20:13:48 -0800

    added order_item validation - needs order_id, stock_item_id and quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,15 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :stock_item
   belongs_to :order
+
+  validates_presence_of :stock_item_id,:quantity,:order_id
+  validate :stock_item_must_exist, :order_must_exist
+
+  def stock_item_must_exist
+    errors.add(:stock_item_id, "this stock_item must exist") if stock_item_id && !StockItem.find(stock_item_id)
+end
+
+  def order_must_exist
+    errors.add(:order_id, "this order must exist") if order_id && !Order.find(order_id)
+  end
 end
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 describe OrderItem do
   before(:each) do
+    @k_order = orders(:kathryn_sf_emeryville_feb3)
+    @s_item = stock_items(:feb3_chickenreg)
     @valid_attributes = {
-      
+      :stock_item_id => @s_item.id,
+      :order_id => @k_order.id,
+      :quantity => 2
     }
   end
 
   it "should create a new instance given valid attributes" do
     OrderItem.create!(@valid_attributes)
   end
+
+  it "should not be valid unless it has valid order_id, stock_item_id and quantity" do    
+    lambda {OrderItem.new(:stock_item_id => 2353135, :order_id => 125125).valid?}.should raise_error
+    OrderItem.new(:stock_item_id => @s_item.id, :order_id => @k_order.id,:quantity=>1).valid?.should == true
+  end
+
 end
