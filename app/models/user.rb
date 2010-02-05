b111df5ac9dab7fff7a87d90e9237850602c3b8c
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-02-04 20:58:29 -0800

    Updated User model, fixtuers, and spec to use new fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   has_many :farms, :through => :subscriptions
   has_many :orders
 
+  validates_presence_of :first_name, :last_name, :username, :email_address, :phone_number, :neighborhood, :password
+
   acts_as_authentic
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe User do
   before(:each) do
     @valid_attributes = {
-      
+      :first_name => "Timothy",
+      :last_name => "Riggins",
+      :username => "DillonFootballRules",
+      :email_address => "tim@riggins.net",
+      :phone_number => "512-353-3694",
+      :password => "gopanthers",
+      :password_confirmation => "gopanthers",
+      :neighborhood => "dillon"
     }
   end
 
