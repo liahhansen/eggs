a0b7d0f9e67d6b47769d8ea7a98ed98f14db0635
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-02-17 23:27:28 -0800

    Fixed a migration, whoops

--- db/migrate/20100218061340_move_farm_and_member_transaction_relationships_to_subscription.rb
+++ db/migrate/20100218061340_move_farm_and_member_transaction_relationships_to_subscription.rb
@@ -1,13 +1,13 @@
 class MoveFarmAndMemberTransactionRelationshipsToSubscription < ActiveRecord::Migration
   def self.up
     remove_column :transactions, :farm_id
-    remove_column :transactions, :memer_id
+    remove_column :transactions, :member_id
     add_column :transactions, :subscription_id, :integer
   end
 
   def self.down
     add_column :transactions, :farm_id, :integer
-    add_column :transactions, :memer_id, :integer
+    add_column :transactions, :member_id, :integer
     remove_column :transactions, :subscription_id
   end
 end
