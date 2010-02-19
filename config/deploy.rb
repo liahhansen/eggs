a6ba5615535410146132473e271a187fc5cf9138
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-02-18 22:47:23 -0800

    testing deploy hook

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,10 +34,14 @@ set :use_sudo, false
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+after "deploy:symlink" do
+  puts "after deploy:symlink"
+end
