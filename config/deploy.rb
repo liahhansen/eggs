07093d23b7380153b4191e4e15c4056b02fe56ad
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-04-03 11:49:28 -0700

    Ok, don't update the crontab for now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,10 +48,3 @@ after "deploy:symlink" do
   run "cp #{File.join(shared_path, 'config', 'production.rb')} #{File.join(current_path, 'config', 'environments', 'production.rb')}"
 end
 
-after "deploy:symlink", "deploy:update_crontab"
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
-end
