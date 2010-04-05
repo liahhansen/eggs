ded73f168dfbc877a2dc7a1da332bf5256b46b54
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-04-05 14:51:09 -0700

    adding backup gem

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "factory_girl", :source => "http://gemcutter.org"
   config.gem "fastercsv"
   config.gem "prawn"
+  config.gem "backup"
 
   
   # Only load the plugins named here, in the order given (default is alphabetical).
