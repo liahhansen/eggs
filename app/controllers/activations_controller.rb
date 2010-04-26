2787ac35bd29a45fd3cf565b493f3f2e3de60bce
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-04-26 13:09:41 -0700

    Extend activation time

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -3,7 +3,7 @@ class ActivationsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
 
   def new
-    @user = User.find_using_perishable_token(params[:activation_code], 2.weeks)
+    @user = User.find_using_perishable_token(params[:activation_code], 12.weeks)
     
     if @user
       if @user.active?
