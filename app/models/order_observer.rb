d1cfa8dc0d344802e25213a067f6c28ae0978992
Kathryn Aaker <kathryn@kathrynaaker.com>
2010-05-12 16:21:53 -0700

    Remove order notes notification - change to trigger from controller

--- app/models/order_observer.rb
+++ app/models/order_observer.rb
@@ -1,7 +1,7 @@
 class OrderObserver < ActiveRecord::Observer
   def after_save(order)
     if order.notes && order.notes != ""
-      Notifier.deliver_order_notes_notification(order)
+      #Notifier.deliver_order_notes_notification(order)
     end
   end
 end
